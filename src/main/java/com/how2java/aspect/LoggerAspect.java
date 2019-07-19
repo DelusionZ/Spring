06b9576e1e6144909cd7b01b78ea2dc65ebd9293
EDZ <https://github.com/DelusionZ/hiworld>
2019-07-19 10:39:28 +0800

    test

--- src/main/java/com/how2java/aspect/LoggerAspect.java
+++ src/main/java/com/how2java/aspect/LoggerAspect.java
@@ -4,7 +4,6 @@ package com.how2java.aspect;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
